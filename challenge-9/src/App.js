5201ebe7887e87614e1726930142c706ff61a74b
UAOMauricio <143342805+UAOMauricio@users.noreply.github.com>
2023-10-11 15:28:34 -0500

    Add files via upload

---
+++ challenge-9/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { GiftGridExpertApp } from './components/gifExpertApp/GifExpertApp';
+
+function App() {
+  return (
+    <div className="App">
+      <GiftGridExpertApp/>
+    </div>
+  );
+}
+
+export default App;
